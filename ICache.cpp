f3455653e899c9c40e0732ad3e4ea10a53ff03d6
VedantS01 <65148655+VedantS01@users.noreply.github.com>
2021-04-21 23:34:37 +0530

    update

---
+++ ICache.cpp
@@ -0,0 +1,17 @@
+/**
+ *  ICache.cpp      :       Implements the class ICache
+ * 
+ *  Version         :       1.0
+ *  Author          :       Vedant Ashish Saboo
+ * 
+ *  NOTE            :       IN DEVELOPEMENT STAGE
+ * 
+ **/
+
+#include "Processor.h"
+#include <iostream>
+using namespace std;
+
+void ICache:: {
+    
+}
